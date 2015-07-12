844b9118c7c222c6f0b8c5efe0df13e718a38709
viru1988 <vijaycse@live.in>
2015-07-12 10:03:11 -0400

    unwanted space removed

--- plot1.R
+++ plot1.R
@@ -5,13 +5,10 @@ library("dplyr")
 fill<-filter(dataset,Date>="2007-02-01" , Date<="2007-02-02")
 #combine data and time
 subset<- mutate(fill,DateTime = paste(Date,Time,sep= ' '))
-
 #Plot1
-
 library(datasets)
 histplot<-hist(as.numeric(subset$Global_active_power), main="Global Active Power", 
                    xlab="Global Active Power (kilowatts)", ylab="Frequency",col="Red")
-
 dev.copy(png,'plot1.png', height=480, width=480)
 dev.off()
 
